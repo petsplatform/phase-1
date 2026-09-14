3dad5ee04bbf8019314ab4e82f440c81fa73f580
Pets E-Commerce Platform <dev.petsplatform@gmail.com>
2026-09-14 18:39:59 +0530

    Clean up accountApi.js by removing comments
    Removed commented out text and cleaned up code.

--- Best-Vet-Care-Frontend/src/api/accountApi.js
+++ Best-Vet-Care-Frontend/src/api/accountApi.js
@@ -1,5 +1,5 @@
 import api from "./axios";
-//hgfdhgdjh
+//hgfdhgdjh-----------hghgfj
 export const accountApi = {
   getDashboard: async () => {
     const res = await api.get("/customer-panel/dashboard");
